b3abcc71d2186798a8cc297baec869f37b4d7d0d
saadthenexus <hasibulkarimsaad@gmail.com>
2024-10-08 10:59:32 +0600

    Product constructor done, thought to implement comparable

--- Product.java
+++ Product.java
@@ -1,3 +1,12 @@
-abstract class Product{
-    
+abstract class Product implements Comparable<Product> {
+    protected String name;
+    protected int stockQuantity;
+    protected double unitPrice;
+
+    public Product(String name, int stockQuantity, double unitPrice) {
+        this.name = name;
+        this.stockQuantity = stockQuantity;
+        this.unitPrice = unitPrice;
+    }
+
 }
